75f956f69e06c0c52e351e7201eefc66367ee062
Murasame <2991086069@qq.com>
2021-06-15 20:59:35 +0800

    逻辑没错了，我彻底下班，准备考试

--- Carcassonne-Game-Server/src/test/java/com/carcassonne/gameserver/manager/RoomManagerTest.java
+++ Carcassonne-Game-Server/src/test/java/com/carcassonne/gameserver/manager/RoomManagerTest.java
@@ -392,7 +392,7 @@ class RoomManagerTest {
             HashMap<Integer,ArrayList<Point>> hashMap = new HashMap<>();
 //            System.out.println("》》》》》》》》》可放坐标周围一圈《《《《《《《《《《");
 //            System.out.println(roomManager.getPuzzle().getCanPutPositionList());
-            System.out.println("手牌:"+cardsArray.get(i).getTop().getType()+","+cardsArray.get(i).getRig().getType()+","+cardsArray.get(i).getBot().getType()+","+cardsArray.get(i).getLef().getType());
+//            System.out.println("手牌:"+cardsArray.get(i).getTop().getType()+","+cardsArray.get(i).getRig().getType()+","+cardsArray.get(i).getBot().getType()+","+cardsArray.get(i).getLef().getType());
 
             hashMap = roomManager.getAllCanPutPositionList(cardsArray.get(i));
 //            System.out.println(hashMap);
@@ -411,31 +411,34 @@ class RoomManagerTest {
 
             Point point = pointArray.get(0);
             Card card = cardsArray.get(i);
-            System.out.println("旋转前:"+card);
+//            System.out.println("旋转前:"+card);
             card.rotate(point.getR());
 //            System.out.println("("+point.getX()+","+point.getY()+")"+","+point.getR());
 //            System.out.println("旋转完:"+card);
             roomManager.putCard(point.getX(),point.getY(),card);
-            System.out.println("当前卡牌:"+cardsArray.get(i).getTop().getType()+cardsArray.get(i).getTop().getCityorroad()+","+cardsArray.get(i).getRig().getType()+cardsArray.get(i).getRig().getCityorroad()+","+cardsArray.get(i).getBot().getType()+cardsArray.get(i).getBot().getCityorroad()+","+cardsArray.get(i).getLef().getType()+cardsArray.get(i).getLef().getCityorroad());
-
-            System.out.println("放在了"+point+point.getR() );
+//            System.out.println("当前卡牌:"+cardsArray.get(i).getTop().getType()+cardsArray.get(i).getTop().getCityorroad()+","+cardsArray.get(i).getRig().getType()+cardsArray.get(i).getRig().getCityorroad()+","+cardsArray.get(i).getBot().getType()+cardsArray.get(i).getBot().getCityorroad()+","+cardsArray.get(i).getLef().getType()+cardsArray.get(i).getLef().getCityorroad());
+//
+//            System.out.println("放在了"+point+point.getR() );
         }
     }
 
     @Test
-    public void gameTestRunMuti(){
-        int k=0;
-        try {
-            for (int  i=0;i<100000;i++){
+    public void gameTestRunMuti() {
+        int k = 0;
+        int a = 0;
+        for (int i = 0; i < 10000; i++) {
+            try {
+
+                a++;
                 gameTest();
                 k++;
-            }
 
-        }catch (Exception e){
+
+            }catch(Exception e){
             System.out.println(e.getMessage());
+            }
         }
-
-        System.out.println("一共对了"+k+"次");
+        System.out.println("总的"+a+",一共对了"+k+"次");
     }
 
     @Test
